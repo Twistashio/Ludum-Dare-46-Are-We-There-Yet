077f752a476e0a9f588c7c04813d7ebc431a261b
Twistashio <lbrodgers16@gmail.com>
2020-04-18 01:37:36 -0400

    Update to project files
    - added new tree model
    -added new car model
    - spread out the level
    -added boredum meter
    -began a simple ui for managing kid
    - scripts that manage ui

---
+++ Unity/LD46/Assets/Scripts/BoringMeter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoringMeter : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public static float slideValue;
+
+
+    float timeToWait = 0.5f;
+    public float fillQuantity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0f;
+        slideValue = 0f;
+
+        StartCoroutine(FillSlider());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = slideValue;
+    }
+
+    public IEnumerator FillSlider()
+    {
+        if (slider.value < 1)
+        {
+            yield return new WaitForSeconds(timeToWait);
+            slideValue += fillQuantity;
+            StartCoroutine(FillSlider());
+            Debug.Log("Done");
+        }
+    }
+}
---
+++ Unity/LD46/Assets/Scripts/BorringManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorringManager : MonoBehaviour
+{
+    public float removeValue = 0.3f;
+
+
+    public void Update()
+    {
+        //incomplete code
+    }
+
+
+    public void Keys()
+    {
+        
+        if (BoringMeter.slideValue > removeValue)
+        {
+            BoringMeter.slideValue = BoringMeter.slideValue - removeValue;
+        }
+    }
+}
