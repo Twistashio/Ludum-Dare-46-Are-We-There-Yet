66881f3b17189e183ffbc1f8ebc87a0b8ce361a2
Twistashio <lbrodgers16@gmail.com>
2020-04-17 23:07:03 -0400

    Movement Script

---
+++ Unity/LD46/Assets/Scripts/CarMovementControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementControls : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+
+    {
+        Controll();
+        rb.AddForce(speed * -6, 0, 0);
+    }
+
+
+    void Controll()
+    {
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.AddForce(0, 0, 20f);
+            Debug.Log("right");
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") > -1)
+            {
+                rb.AddForce(0, 0, -20f);
+                Debug.Log("left");
+            }
+            else
+            {
+                rb.AddForce(speed * -6, 0, 0);
+
+            }
+        }
+    }
+}
---
+++ Unity/LD46/Assets/Scripts/DrivingControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingControler : MonoBehaviour
+{
+
+   public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       rb.AddForce(-50 * Time.deltaTime,0,0);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0,0, 300 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -300 * Time.deltaTime);
+        }
+    }
+}
