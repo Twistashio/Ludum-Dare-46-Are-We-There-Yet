7aa5c21a899e6b6eb8a747b0c09ae301a3f8331e
Twistashio <lbrodgers16@gmail.com>
2020-04-17 23:39:29 -0400

    Revised Movement

--- Unity/LD46/Assets/Scripts/DrivingControler.cs
+++ Unity/LD46/Assets/Scripts/DrivingControler.cs
@@ -6,27 +6,36 @@ public class DrivingControler : MonoBehaviour
 {
 
    public Rigidbody rb;
-   public float Start_Speed = 50;
-   public float Acceleration = 50;
+
+
+   public float startSpeed;
+   public float Acceleration;
+   public float tiltSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity=new Vector3(-Start_Speed,0,0);
+        rb.velocity=new Vector3(-startSpeed,0,0);
+
+        // Multiplies the lane switching speed so we can imput lower values into the editor
+        tiltSpeed = tiltSpeed * 10;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+       
+
        rb.AddForce(-Acceleration * Time.deltaTime,0,0);
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(0,0, 300 * Time.deltaTime);
+            rb.AddForce(0,0, tiltSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(0, 0, -300 * Time.deltaTime);
+            rb.AddForce(0, 0, -tiltSpeed * Time.deltaTime);
         }
     }
 }
