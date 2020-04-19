0bb4da78c7c09245651a5728ba2ce2a13fe51e14
Twistashio <lbrodgers16@gmail.com>
2020-04-19 18:54:27 -0400

    Tester Scene

--- Unity/LD46/Assets/Scripts/BorringManager.cs
+++ Unity/LD46/Assets/Scripts/BorringManager.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class BorringManager : MonoBehaviour
 {
     public float removeValue = 0.3f;
+    private float value = 3f;
 
     //state of the menu closed/open
     public static bool boringMenu;
+    public static bool keysPressed;
 
     public GameObject boringMenuUI;
+    public Button uiButton;
 
     public Animation forward;
     public Animation backward;
@@ -71,9 +74,55 @@ public class BorringManager : MonoBehaviour
 
     public void Keys()
     {
+
+            if (keysPressed == true && DrivingControler.CanMove == false)
+            {
+                BoringMeter.slideValue = BoringMeter.slideValue - removeValue;
+                keysPressed = false;
+                keySlider.value = 0f;
+                value = 3f;
+            uiButton.interactable = true;
+
+            }
+            else
+            {
+                if (DrivingControler.CanMove == false)
+                {
+                    StartCoroutine(keyCounter());
+                uiButton.interactable = false;
+                
+                }
+                else
+                {
+                    keySlider.value = 0f;
+                uiButton.interactable = true;
+                }
+            }
+
+        }
+    
+    
+    IEnumerator keyCounter()
+    {
+
         
-      
-            BoringMeter.slideValue = BoringMeter.slideValue - removeValue;
+
+        yield return new WaitForSeconds(0.3f);
+        keySlider.value += 0.3f;
+
+        if (keySlider.value == 1f)
+        {
+            
+            keysPressed = true;
+            Keys();
+        }
+        else
+        {
+            Keys();
+
+        }
         
+
+
     }
 }
---
+++ Unity/LD46/Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    public GameObject GameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameUI.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameUI.SetActive(true);
+    }
+}
