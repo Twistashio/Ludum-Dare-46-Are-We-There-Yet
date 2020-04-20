c00f82d132f3837b493479dec1b2f3251a85dee6
Twistashio <lbrodgers16@gmail.com>
2020-04-20 10:14:04 -0400

    test

---
+++ Unity/LD46/Assets/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public DrivingControler movement;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+       if (collisionInfo.collider.tag == "Hills")
+        {
+            movement.enabled = false;
+
+            Debug.Log("We hit an obsticle");
+        }
+    }
+}
